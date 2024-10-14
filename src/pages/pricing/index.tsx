c12d0a2c9695f59b8c80cf876fef1688b1032fb4
Its Rohith <rohit311710@gmail.com>
2024-10-14 15:53:04 +0530

    Add: pricing page

---
+++ src/pages/pricing/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '@/components/others/Footer'
+import Navbar from '@/components/others/NavBar'
+import Pricing from '@/components/home/Pricing'
+
+const index = () => {
+    return (
+        <div>
+            <Navbar />
+            <Pricing />
+            <Footer />
+        </div>
+    )
+}
+
+export default index
